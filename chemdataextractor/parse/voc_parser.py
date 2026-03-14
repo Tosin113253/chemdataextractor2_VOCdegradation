54aea23944e0ae9063a178f63cd63032dfc7e682
Tosin113253 <105084603+Tosin113253@users.noreply.github.com>
2026-03-14 11:02:42 -0400

    Update voc_parser.py

--- chemdataextractor/parse/voc_parser.py
+++ chemdataextractor/parse/voc_parser.py
@@ -1,27 +1,29 @@
-from chemdataextractor.parse.elements import W, I, R
-from chemdataextractor.parse.actions import merge
+from chemdataextractor.parse.elements import W, I, R, Optional
 from chemdataextractor.parse.base import BaseParser
-from chemdataextractor.model.voc_photocatalysis import VOCPhotocatalysis
 
 
 class DegradationEfficiencyParser(BaseParser):
 
-    # match numbers like 95%
-    efficiency_value = R(r'^\d+(\.\d+)?$') + W('%')
+    number = R(r'^\d+(\.\d+)?$')
+    percent = W('%')
 
-    # keywords around degradation
     degradation_words = (
         I('degradation') |
         I('removal') |
         I('conversion')
     )
 
-    root = efficiency_value('degradation_efficiency') + degradation_words
+    efficiency_word = Optional(I('efficiency'))
+
+    root = (
+        number('value') + percent + degradation_words + efficiency_word
+    ) | (
+        degradation_words + efficiency_word + Optional(I('of')) + number('value') + percent
+    )
 
     def interpret(self, result, start, end):
+        from chemdataextractor.model.voc_photocatalysis import VOCPhotocatalysis
 
         record = VOCPhotocatalysis()
-
-        record.degradation_efficiency = " ".join(result.degradation_efficiency)
-
+        record.degradation_efficiency = f"{result.value.text}%"
         yield record
