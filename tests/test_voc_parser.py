568bfc2fef3cdb391e238535314c393942001cff
Tosin113253 <105084603+Tosin113253@users.noreply.github.com>
2026-03-14 12:19:32 -0400

    Update test_voc_parser.py

--- tests/test_voc_parser.py
+++ tests/test_voc_parser.py
@@ -1,8 +1,14 @@
+print("START")
+
 from chemdataextractor import Document
 
 text = "95 % degradation efficiency was achieved."
+print("TEXT LOADED")
 
 doc = Document(text)
+print("DOC CREATED")
 
 for record in doc.records:
     print(record.serialize())
+
+print("END")
