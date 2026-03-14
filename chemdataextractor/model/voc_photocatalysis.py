883f86c070a46ea97fa6f7350b348859b8586917
Tosin113253 <105084603+Tosin113253@users.noreply.github.com>
2026-03-14 11:03:26 -0400

    Update voc_photocatalysis.py

--- chemdataextractor/model/voc_photocatalysis.py
+++ chemdataextractor/model/voc_photocatalysis.py
@@ -1,30 +1,19 @@
 from .base import BaseModel, StringType
 from ..parse.voc_parser import DegradationEfficiencyParser
 
+
 class VOCPhotocatalysis(BaseModel):
-
     pollutant = StringType()
-
     catalyst = StringType()
-
     initial_concentration = StringType()
-
     wavelength = StringType()
-
     dosage = StringType()
-
     reactor_volume = StringType()
-
     light_intensity = StringType()
-
     reaction_time = StringType()
-
     degradation_efficiency = StringType()
-
     temperature = StringType()
-
     humidity = StringType()
-
     k_rate_constant = StringType()
 
     parsers = [DegradationEfficiencyParser()]
